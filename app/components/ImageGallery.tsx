70ce8954d41e46d13cfdf15b8d1ec0c0c3d43d19
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-12-13 12:18:44 +0100

    product page, ImageGallery modified

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -14,65 +14,81 @@ import 'swiper/css/navigation'
 import 'swiper/css/thumbs'
 
 interface iAppProps {
-    images: StaticImport[],
+  images: StaticImport[],
 }
 
 const ImageGallery = ({ images }: iAppProps) => {
-    const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null)
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null)
 
-    return (
-      <section className='min-h-screen py-12'>
-        <div className='container'>
-          <Swiper
-            spaceBetween={10}
-            navigation={true}
-            thumbs={{
-              swiper:
-                thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
-            }}
-            modules={[FreeMode, Navigation, Thumbs]}
-            className='h-96 w-full rounded-lg'
-          >
-            {images.map((image: string | StaticImport, index: number) => (
-              <SwiperSlide key={index}>
-                <div className='flex h-full w-full items-center justify-center'>
-                  <Image
-                    src={urlFor(image).url()}
-                    width={500} height={500}
-                    alt='photo'
-                    className='block h-full w-full object-cover'
-                  />
+  return (
+    <section className='sm:min-h-screen sm:py-12'>
+      <div className='container flex flex-col'>
+        <Swiper
+          spaceBetween={10}
+          navigation={true}
+          thumbs={{
+            swiper:
+              thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+          }}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className='w-64 sm:h-96 sm:w-full rounded-lg'
+        >
+          {images.map((image: string | StaticImport, index: number) => (
+            <SwiperSlide key={index}>
+              <div className='flex h-full w-full items-center justify-center'>
+                <Image
+                  src={urlFor(image).url()}
+                  width={500} height={500}
+                  alt='photo'
+                  className='block h-full w-full object-cover'
+                  onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}
+                />
+              </div>
+              <dialog id="my_modal_3" className="modal">
+                <div className="modal-box w-2/5 max-w-5xl bg-white">
+                  <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-3 top-3">✕</button>
+                  </form>
+                  <p className="py-4">
+                    <Image
+                      src={urlFor(image).url()}
+                      width={600} height={600}
+                      alt='photo'
+                      className='block h-full w-full object-cover'
+                    />
+                  </p>
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-  
-          {/* Thumbnail */}
-          <Swiper
-            onSwiper={setThumbsSwiper}
-            spaceBetween={12}
-            slidesPerView={4}
-            freeMode={true}
-            watchSlidesProgress={true}
-            modules={[FreeMode, Navigation, Thumbs]}
-            className='thumbs mt-3 h-32 w-full rounded-lg'
-          >
-            {images.map((image: string | StaticImport, index: number) => (
-              <SwiperSlide key={index}>
-                <button className='flex h-full w-full items-center justify-center'>
-                  <Image
-                    src={urlFor(image).url()}
-                    width={200} height={200}
-                    alt='Photo' 
-                    className='block h-full w-full object-cover'
-                  />
-                </button>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </section>
-    )
+              </dialog>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/* Thumbnail */}
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={12}
+          slidesPerView={4}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className='thumbs mt-3 h-32 w-full rounded-lg'
+        >
+          {images.map((image: string | StaticImport, index: number) => (
+            <SwiperSlide key={index}>
+              <button className='flex h-full w-full items-center justify-center'>
+                <Image
+                  src={urlFor(image).url()}
+                  width={200} height={200}
+                  alt='Photo'
+                  className='block h-16 sm:h-full sm:w-full object-cover'
+                />
+              </button>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  )
 }
 
 export default ImageGallery
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,8 +1,7 @@
 
 import ImageGallery from '@/app/components/ImageGallery';
 import { client } from '@/app/lib/sanity'
-import { Button } from '@/components/ui/button';
-import { Star, Truck } from 'lucide-react';
+import { Truck } from 'lucide-react';
 import React from 'react'
 
 export interface fullProduct {
@@ -49,24 +48,13 @@ const ProductPage = async ({ params }: { params: { slug: string } }) => {
                             <h2 className='text-2xl font-bold text-gray-800 lg:text-3xl'>{data.name}</h2>
                         </div>
 
-                        <div className='mb-6 flex items-center gap-3 md:mb-10'>
-                            <Button className='rounded-full gap-x-2'>
-                                <span className='text-sm'>4.2</span>
-                                <Star className='h-5 w-5' />
-                            </Button>
-
-                            <span className='text-sm text-gray-500 transition duration-100'>
-                                56 Ratings
-                            </span>
-                        </div>
-
                         <div className='mb-4'>
                             <div className='flex item-end gap-2'>
                                 <span className='text-xl font-bold text-gray-800 md:text-2xl'>
                                     ${data.price}
                                 </span>
                                 <span className='mb-0.5 text-red-500 line-through'>
-                                    ${data.price + 30}
+                                    ${data.price * 0.8}
                                 </span>
                             </div>
 
