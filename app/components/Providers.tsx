1c116642cd4f5ba255e9a2514c7f9b5b5d57f726
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-12-31 11:05:10 +0100

    bug fix with stripe and sanity

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -10,8 +10,8 @@ const CartProvider = ({children}: {children: ReactNode}) => {
                 mode='payment' 
                 cartMode='client-only' 
                 stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-                successUrl='http://localhost:3000/stripe/success'
-                cancelUrl='http://localhost:3000/stripe/error'
+                successUrl='https://ecommerce-shoe-store-pi.vercel.app/stripe/success'
+                cancelUrl='https://ecommerce-shoe-store-pi.vercel.app/stripe/error'
                 currency='USD'
                 billingAddressCollection={true}
                 shouldPersist={true}
